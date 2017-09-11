e61cfb7f7d0b84b64519667efdeb8c27776b5091
Sharang Malhotra <sharang.2795@gmail.com>
2017-09-11 16:29:52 +0530

    Add files via upload

--- frontend/src/app/user-profile/user-profile.component.ts
+++ frontend/src/app/user-profile/user-profile.component.ts
@@ -4,8 +4,8 @@ import { LoginService } from './../service/login.service';
 
 @Component({
   selector: 'app-user-profile',
-  templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.css']
+  templateUrl: './user-profile.component.html'
+ // styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit {
 
--- frontend/src/app/user-signup/user-signup.component.ts
+++ frontend/src/app/user-signup/user-signup.component.ts
@@ -11,8 +11,8 @@ import { User } from '../models/user.model';
 
 @Component({
   selector: 'app-user-signup',
-  templateUrl: './user-signup.component.html',
-  styleUrls: ['./user-signup.component.css']
+  templateUrl: './user-signup.component.html'
+ // styleUrls: ['./user-signup.component.css']
 })
 export class UserSignupComponent implements OnInit {
 
